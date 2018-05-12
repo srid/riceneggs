3a2ac9527e2a41f82e6e67b48079c77926a0a7bd
Sridhar Ratnakumar <srid@srid.ca>
2018-05-12 12:39:16 -0400

    Rough impl

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -1,18 +1,35 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Frontend where
+
+import Control.Applicative (liftA2)
+import Data.Default (def)
+import Data.Profunctor (lmap)
+import qualified Data.Text as T
+import Text.Read (readMaybe)
 
 import Reflex.Dom
 
-import qualified Data.Text as T
-
 import Static
-import Common.Api
 
 frontend :: Widget x ()
 frontend = do
-  text "Welcome to Obelisk!"
-  el "p" $ text $ T.pack commonStuff
-  elAttr "img" ("src" =: static @"obelisk.jpg") blank
+  el "h1" $ text "Nutrition calculator"
+  eggs <- el "div" $ do
+    el "label" $ text "Eggs"
+    value <$> textInput def
+  rice <- el "div" $ do
+    el "label" $ text "Rice (grams)"
+    value <$> textInput def
 
+  let eggs'  = fmap (readMaybe . T.unpack) eggs
+  let rice'  = fmap (readMaybe . T.unpack)  rice
+  let total = fmap (T.pack . show) $ zipDynWith (liftA2 calc) eggs' rice'
+
+  el "div" $ do
+    el "b" $ text "Total calories:"
+    dynText $ total
+  return ()
+
+calc :: Fractional a => a -> a -> a
+calc eggs riceGrams = eggs * 78 + riceGrams * 3.65
