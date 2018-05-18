905c87b48f986730cdffb3b366a0dd6cda32fd10
Sridhar Ratnakumar <srid@srid.ca>
2018-05-18 09:12:21 -0400

    ap

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 with import ./.obelisk/impl { inherit system; };
 project ./. ({ ... }: {
   android.applicationId = "ca.srid.riceneggs";
-  android.displayName = "Rice 'n eggs";
+  android.displayName = "Rice \'n eggs";
   ios.bundleIdentifier = "ca.srid.riceneggs";
   ios.bundleName = "Rice 'n eggs";
 })
