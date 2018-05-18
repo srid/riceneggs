8edb476cfbb81138b376c8c7f27dd87e0b0dbf8d
Sridhar Ratnakumar <srid@srid.ca>
2018-05-18 09:07:45 -0400

    change android id

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 { system ? builtins.currentSystem }: # TODO: Get rid of this system cruft
 with import ./.obelisk/impl { inherit system; };
 project ./. ({ ... }: {
-  android.applicationId = "systems.obsidian.obelisk.examples.minimal";
-  android.displayName = "Obelisk Minimal Example";
-  ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
-  ios.bundleName = "Obelisk Minimal Example";
+  android.applicationId = "ca.srid.riceneggs";
+  android.displayName = "Rice 'n eggs";
+  ios.bundleIdentifier = "ca.srid.riceneggs";
+  ios.bundleName = "Rice 'n eggs";
 })
